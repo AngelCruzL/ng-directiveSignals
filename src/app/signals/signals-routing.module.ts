41f20d556fdac8259d6d77949a7173764d5e6677
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-09-20 00:14:44 -0600

    Add signals pages routes

---
+++ src/app/signals/signals-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignalsComponent } from './signals.component';
+import { CounterComponent } from './pages/counter/counter.component';
+import { UserInfoComponent } from './pages/user-info/user-info.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignalsComponent,
+    children: [
+      { path: 'counter', component: CounterComponent },
+      { path: 'user-info', component: UserInfoComponent },
+      { path: 'properties', component: PropertiesComponent },
+      { path: '**', redirectTo: 'counter', pathMatch: 'full' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class SignalsRoutingModule {}
---
+++ src/app/signals/signals.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignalsRoutingModule } from './signals-routing.module';
+import { SignalsComponent } from './signals.component';
+import { CounterComponent } from './pages/counter/counter.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
+import { UserInfoComponent } from './pages/user-info/user-info.component';
+
+@NgModule({
+  declarations: [SignalsComponent, CounterComponent, PropertiesComponent, UserInfoComponent],
+  imports: [CommonModule, SignalsRoutingModule],
+})
+export class SignalsModule {}
