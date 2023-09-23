f5db984c6d421a316ff64ae3b9a2a3452465f4e3
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-09-23 08:17:54 -0600

    Add signals counter example

--- src/app/signals/pages/counter/counter.component.ts
+++ src/app/signals/pages/counter/counter.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent {
+  counter = signal(10);
+  squareCounter = computed(() => this.counter() ** 2);
 
+  increaseBy(value: number) {
+    this.counter.update(current => current + value);
+  }
 }
