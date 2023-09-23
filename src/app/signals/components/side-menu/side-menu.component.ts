7e7c9a00683746b8b457731b720eb8ba4f782cb4
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-09-22 23:30:44 -0600

    Add side-menu component

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+type MenuItem = {
+  title: string;
+  route: string;
+};
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss'],
+})
+export class SideMenuComponent {
+  menuItems: MenuItem[] = [
+    { title: 'Contador', route: 'counter' },
+    { title: 'Usuario', route: 'user-info' },
+    { title: 'Mutaciones', route: 'properties' },
+  ];
+}
--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -6,9 +6,10 @@ import { SignalsComponent } from './signals.component';
 import { CounterComponent } from './pages/counter/counter.component';
 import { PropertiesComponent } from './pages/properties/properties.component';
 import { UserInfoComponent } from './pages/user-info/user-info.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
-  declarations: [SignalsComponent, CounterComponent, PropertiesComponent, UserInfoComponent],
+  declarations: [SignalsComponent, CounterComponent, PropertiesComponent, UserInfoComponent, SideMenuComponent],
   imports: [CommonModule, SignalsRoutingModule],
 })
 export class SignalsModule {}
